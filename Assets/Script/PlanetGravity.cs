520f90796c51bcb2dd2160a0229b369570f3a90e
Symon799 <simonrigard@gmail.com>
2016-12-26 01:43:32 +0100

    Added Unity Project
    - Added character controller systeme for planets
    - Added 2 camera modes for testing
    - Controller support
    - Added temporary Sprites

---
+++ Assets/Script/PlanetGravity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGravity : MonoBehaviour {
+
+    float maxGravDist = 45f;
+    //float maxGrav = 45f;
+    private GameObject[] planets;
+
+	void Start ()
+    {
+        planets = GameObject.FindGameObjectsWithTag("Planet");
+	}
+	
+	void Update ()
+    {
+
+        Quaternion rotation = Quaternion.LookRotation (planets[0].transform.position - transform.position, transform.TransformDirection(Vector3.forward));
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+        foreach (GameObject p in planets)
+        {
+            float dist = Vector3.Distance(p.transform.position, transform.position);
+            if (dist <= maxGravDist)
+            {
+                var v = p.transform.position - transform.position;
+                GetComponent<Rigidbody2D>().AddForce(v.normalized * (1.0f - dist / maxGravDist) * maxGravDist);
+            }
+        }
+	}
+}
