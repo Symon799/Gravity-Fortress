1ac889a0957d3df64620254efb91f4ae78b59805
Symon799 <simonrigard@gmail.com>
2017-01-19 01:45:16 +0100

    Arrow shoot & gravity
    The arrow is now shot according to the gravity of the planet

--- Assets/Script/MoveScript.cs
+++ Assets/Script/MoveScript.cs
@@ -2,31 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveScript : MonoBehaviour {
+public class MoveScript : MonoBehaviour
+{
 
-    public Vector2 speed = new Vector2(5, 5);
     public Vector2 direction = new Vector2(1, 0);
+    float maxGravDist = 45f;
+    private GameObject[] planets;
 
-    private Vector2 movement;
-    private Rigidbody2D rigidbodyComponent;
-
-	// Use this for initialization
-	void Start () {
-        rigidbodyComponent = GetComponent<Rigidbody2D>();
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        movement = new Vector2(speed.x * direction.x, speed.y * direction.y);
-        
-    }
-
-    private void FixedUpdate()
+    // Use this for initialization
+    void Start()
     {
-        if (rigidbodyComponent == null)
+        planets = GameObject.FindGameObjectsWithTag("Planet");
+        GetComponent<Rigidbody2D>().AddForce(transform.right * 2500f);
+    }
+
+    void Update()
+    {
+        foreach (GameObject p in planets)
         {
-            rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+            Quaternion rotation = Quaternion.LookRotation(planets[0].transform.position - transform.position, transform.TransformDirection(Vector3.forward));
+            transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+            //float dist = Vector3.Distance(p.transform.position, transform.position);
+            //if (dist <= maxGravDist)
+            
         }
-        rigidbodyComponent.velocity = movement;
     }
 }
