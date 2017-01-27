adfcd58a31c02f4839eda67602877f0078e07a9b
Symon799 <simonrigard@gmail.com>
2017-01-27 15:24:08 +0100

    Fixed multiplayer
    fixed arrow bug (arrowMulti prefab)
    fixed CameraFollow

--- Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -5,7 +5,7 @@ namespace UnityStandardAssets._2D
 {
     public class Camera2DFollow : MonoBehaviour
     {
-        public GameObject target = null;
+        public GameObject target;
         public float damping = 1;
         public float lookAheadFactor = 3;
         public float lookAheadReturnSpeed = 0.5f;
@@ -19,33 +19,43 @@ namespace UnityStandardAssets._2D
         // Use this for initialization
         private void Start()
         {
-            m_LastTargetPosition = target.transform.position;
-            m_OffsetZ = (transform.position - target.transform.position).z;
-            transform.parent = null;
+            target = GameObject.FindGameObjectWithTag("Player");
+            if (target)
+            {
+                m_LastTargetPosition = target.transform.position;
+                m_OffsetZ = (transform.position - target.transform.position).z;
+                transform.parent = null;
+            }
         }
 
 
         // Update is called once per frame
         private void Update()
         {
-            if (target == null)
-                target = GameObject.FindGameObjectWithTag("Player");
             // only update lookahead pos if accelerating or changed direction
-            float xMoveDelta = (target.transform.position - m_LastTargetPosition).x;
+            target = GameObject.FindGameObjectWithTag("Player");
+            if (target)
+            {
+                float xMoveDelta = (target.transform.position - m_LastTargetPosition).x;
 
-            bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+                bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
 
-            if (updateLookAheadTarget)
-                m_LookAheadPos = lookAheadFactor*Vector3.right*Mathf.Sign(xMoveDelta);
-            else
-                m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
+                if (updateLookAheadTarget)
+                {
+                    m_LookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(xMoveDelta);
+                }
+                else
+                {
+                    m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);
+                }
 
-            Vector3 aheadTargetPos = target.transform.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
-            Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
+                Vector3 aheadTargetPos = target.transform.position + m_LookAheadPos + Vector3.forward * m_OffsetZ;
+                Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
-            transform.position = newPos;
+                transform.position = newPos;
 
-            m_LastTargetPosition = target.transform.position;
+                m_LastTargetPosition = target.transform.position;
+            }
         }
     }
 }
